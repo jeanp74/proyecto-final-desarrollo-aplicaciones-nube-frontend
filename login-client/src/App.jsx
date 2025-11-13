08533775b77aa6ebacbd1d3aa7d56ea1acad450d
jeanp74 <jeanpaulsolano74@gmail.com>
2025-11-13 18:20:52 -0500

    Se coloco un alert para poner mostrar la data qeu devuelve el api gateway

--- login-client/src/App.jsx
+++ login-client/src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       console.log("Login correcto:", data);
 
       // Guardar token y redirigir
+      alert(data);
       localStorage.setItem("token", data.token);
       localStorage.setItem("user", JSON.stringify(data.user));
 
