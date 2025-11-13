fca0cdedd43f82bb42edc6457b57cfc590b7885c
jeanp74 <jeanpaulsolano74@gmail.com>
2025-11-13 09:17:22 -0500

    Actualizar la url del api gateway

--- login-client/src/api.js
+++ login-client/src/api.js
@@ -4,7 +4,7 @@ const LOGIN_KEY = "login_react_api_base";
 
 const LOGIN_DEFAULT =
   import.meta.env.VITE_API_LOGIN_BASE ||
-  "api-gateway-proyecto-final-desarrollo-buf8gkfvfbg7aag4.brazilsouth-01.azurewebsites.net"; // URL de tu gateway en Azure
+  "https://api-gateway-proyecto-final-desarrollo-buf8gkfvfbg7aag4.brazilsouth-01.azurewebsites.net/"; // URL de tu gateway en Azure
 
 function joinUrl(base, path) {
   const b = (base || "/").replace(/\/+$/, "");
