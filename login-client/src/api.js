218015db2b349f9da8cc8f1c0e9315269308d8b7
jeanp74 <164540906+jeanp74@users.noreply.github.com>
2025-11-12 21:27:28 -0500

    SE MODIFICO API.JS POPR QUE LA URL ESTAAB MAL

--- login-client/src/api.js
+++ login-client/src/api.js
@@ -4,7 +4,7 @@ const LOGIN_KEY = "login_react_api_base";
 
 const LOGIN_DEFAULT =
   import.meta.env.VITE_API_LOGIN_BASE ||
-  "https://api-gateway-proyecto-final.azurewebsites.net"; // URL de tu gateway en Azure
+  "api-gateway-proyecto-final-desarrollo-buf8gkfvfbg7aag4.brazilsouth-01.azurewebsites.net"; // URL de tu gateway en Azure
 
 function joinUrl(base, path) {
   const b = (base || "/").replace(/\/+$/, "");
