b52063f06606a1048183199ce191804fd692fc2d
Deyvid Santiago Galarza <deyvidgalarzag04@gmail.com>
2025-11-15 19:10:23 -0500

    appointments9

--- appointments-client/src/api.js
+++ appointments-client/src/api.js
@@ -15,7 +15,7 @@ const PATIENTS_DEFAULT =
   import.meta.env.VITE_PATIENTS_API_BASE || process.env.VITE_PATIENTS_API_BASE || "https://patients-proyecto-final-desarrollo-dvdpe0eegng6atfy.brazilsouth-01.azurewebsites.net/";
 
 const DOCTORS_DEFAULT =
-  import.meta.env.VITE_DOCTORS_API_BASE || process.env.VITE_DOCTORS_API_BASE || "https://doctors-proyecto-final-desarrollo-xyz.brazilsouth-01.azurewebsites.net/";
+  import.meta.env.VITE_DOCTORS_API_BASE || process.env.VITE_DOCTORS_API_BASE || "https://doctors-proyecto-final-desarrollo-b7aqbdbpcgd0d7bq.brazilsouth-01.azurewebsites.net/";
 
 // ---- Utils ----
 function joinUrl(base, path) {
