3f7a1b06f177da96ed387cea23cc90a48706d639
jeanp74 <jeanpaulsolano74@gmail.com>
2025-11-18 18:26:08 -0500

    VARIABLE DE ENTORNO 3

--- login-client/src/api.js
+++ login-client/src/api.js
@@ -3,7 +3,8 @@
 const LOGIN_KEY = "login_react_api_base";
 
 const LOGIN_DEFAULT = process.env.GATEWAY || import.meta.env.GATEWAY; // URL de tu gateway en Azure
-alert(LOGIN_DEFAULT);
+console.log(process.env);
+
 
 function joinUrl(base, path) {
   const b = (base || "/").replace(/\/+$/, "");
