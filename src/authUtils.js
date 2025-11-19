c819a69a6ff003b2f5e33809ace68e79b5b28221
jeanp74 <jeanpaulsolano74@gmail.com>
2025-11-19 14:38:17 -0500

    9

--- src/authUtils.js
+++ src/authUtils.js
@@ -1,7 +1,9 @@
 import { useEffect } from "react";
 
+const VITE_GATEWAY = import.meta.env.VITE_GATEWAY;
 const Gateway = import.meta.env.VITE_GATEWAY;
-console.log("URL Gateway: ", Gateway);
+console.log("URL gateway: ", VITE_GATEWAY);
+console.log("URL gateway22: ", Gateway);
 
 // Llama al backend para renovar el token
 export async function refreshToken() {
@@ -9,13 +11,13 @@ export async function refreshToken() {
   if (!refresh) return null;
 
   try {
-    const resp = await fetch(`${Gateway}/auth/refresh`, {
+    const resp = await fetch(`${VITE_GATEWAY}/auth/refresh`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ refresh_token: refresh }),
     });
 
-    console.log("Gateway: ", Gateway);
+    console.log("URL gateway2: ", VITE_GATEWAY);
     console.log("Refrescar token");
 
     const data = await resp.json();
@@ -51,7 +53,7 @@ export async function getAllowedModules() {
   if (!access) return null;
 
   try {
-    const resp = await fetch(`${Gateway}/api/modules`, {
+    const resp = await fetch(`${VITE_GATEWAY}/api/modules`, {
       method: "GET",
       headers: { 
         "Content-Type": "application/json",
